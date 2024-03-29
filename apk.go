875c72296ccb7883310bb428fe0aedfe515db7a7
zhiqiangwang <zhiqiangwang@aliyun.com>
2024-03-30 00:22:49 +0800

    feat: apk

---
+++ apk.go
@@ -0,0 +1,52 @@
+package appgo
+
+import (
+	"io"
+	"os"
+
+	"github.com/pkg6/appgo/seekbuf"
+	"github.com/shogo82148/androidbinary"
+	"github.com/shogo82148/androidbinary/apk"
+)
+
+func APKParsePath(file string) (*APK, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return APKParseFile(f)
+}
+
+func APKParseFile(f *os.File) (*APK, error) {
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	buf, err := seekbuf.Open(f, seekbuf.MemoryMode)
+	if err != nil {
+		return nil, err
+	}
+	defer buf.Close()
+	return APKParseReaderAt(buf, fi.Size())
+}
+func APKParseReaderAt(readerAt io.ReaderAt, size int64) (*APK, error) {
+	pkg, err := apk.OpenZipReader(readerAt, size)
+	if err != nil {
+		return nil, err
+	}
+	defer pkg.Close()
+
+	icon, err := pkg.Icon(&androidbinary.ResTableConfig{
+		Density: 720,
+	})
+	if err != nil {
+		// NOTE: ignore error
+	}
+
+	return &APK{
+		icon:     icon,
+		manifest: pkg.Manifest(),
+		size:     size,
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pkg6/appgo
 
 go 1.18
 
-require github.com/pkg6/go-plist v0.0.0-20240329135841-30c4eb2341fd // indirect
+require (
+	github.com/pkg6/go-plist v0.0.0-20240329135841-30c4eb2341fd
+	github.com/shogo82148/androidbinary v1.0.5
+)
